4001a038b89db5aa18cd1f7eaebbbc5a9c53796d
atomita <sleeping.cait.sith+gh@gmail.com>
2015-10-24 20:57:51 +0900

    to suppress "warning: toplevel constant Elasticsearch referenced"

--- lib/fluent/plugin/out_aws-elasticsearch-service.rb
+++ lib/fluent/plugin/out_aws-elasticsearch-service.rb
@@ -69,50 +69,67 @@ module Fluent
         end
     end
 
+  end
 
-    #
-    # monkey patch
-    #
-    class ElasticsearchOutput::Elasticsearch::Transport::Transport::HTTP::Faraday
 
-      alias :__build_connections_origin_from_aws_elasticsearch_service_output :__build_connections
+  #
+  # monkey patch
+  #
+  class ElasticsearchOutput
+    module Elasticsearch
 
-      # Builds and returns a collection of connections.
-      #
-      # @return [Connections::Collection]
-      # @override
-      #
-      def __build_connections
-        Elasticsearch::Transport::Transport::Connections::Collection.new(
-          :connections => hosts.map { |host|
-            host[:protocol]   = host[:scheme] || DEFAULT_PROTOCOL
-            host[:port]     ||= DEFAULT_PORT
-            url               = __full_url(host)
-
-            Elasticsearch::Transport::Transport::Connections::Connection.new(
-              :host => host,
-              :connection => ::Faraday::Connection.new(
-                url,
-                (options[:transport_options] || {}),
-                &__aws_elasticsearch_service_setting(host, &@block)
-              ),
-              :options => host[:connection_options]
-            )
-          },
-          :selector_class => options[:selector_class],
-          :selector => options[:selector]
-        )
+      module Client
+        include ::Elasticsearch::Client
+        extend self
       end
 
-      def __aws_elasticsearch_service_setting(host, &block)
-        lambda do |faraday|
-          if host[:aws_elasticsearch_service]
-            faraday.request :aws_signers_v4,
-                            credentials: host[:aws_elasticsearch_service][:credentials],
-                            service_name: 'es',
-                            region: host[:aws_elasticsearch_service][:region]
+      module Transport
+        module Transport
+          module HTTP
+            class Faraday < ::Elasticsearch::Transport::Transport::HTTP::Faraday
+
+              alias :__build_connections_origin_from_aws_elasticsearch_service_output :__build_connections
+
+              # Builds and returns a collection of connections.
+              #
+              # @return [Connections::Collection]
+              # @override
+              #
+              def __build_connections
+                ::Elasticsearch::Transport::Transport::Connections::Collection.new(
+                  :connections => hosts.map { |host|
+                    host[:protocol]   = host[:scheme] || DEFAULT_PROTOCOL
+                    host[:port]     ||= DEFAULT_PORT
+                    url               = __full_url(host)
+
+                    ::Elasticsearch::Transport::Transport::Connections::Connection.new(
+                      :host => host,
+                      :connection => ::Faraday::Connection.new(
+                        url,
+                        (options[:transport_options] || {}),
+                        &__aws_elasticsearch_service_setting(host, &@block)
+                      ),
+                      :options => host[:connection_options]
+                    )
+                  },
+                  :selector_class => options[:selector_class],
+                  :selector => options[:selector]
+                )
+              end
+
+              def __aws_elasticsearch_service_setting(host, &block)
+                lambda do |faraday|
+                  if host[:aws_elasticsearch_service]
+                    faraday.request :aws_signers_v4,
+                                    credentials: host[:aws_elasticsearch_service][:credentials],
+                                    service_name: 'es',
+                                    region: host[:aws_elasticsearch_service][:region]
+                  end
+                  block.call faraday
+                end
+              end
+            end
           end
-          block.call faraday
         end
       end
     end
